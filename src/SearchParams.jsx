22576a1eeb96835274705f6622e9336a7d0ac768
pabloppizarro <pizarro.pablo@outlook.com>
2023-08-17 12:30:26 -0300

    initial commit project files

---
+++ src/SearchParams.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
+const ANIMALS = ["bird", "cat", "dog", "reptile"];
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState([]);
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    setPets(json.pets);
+  }
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              // setBreed("");
+            }}
+          >
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          <select
+            disabled={breed.length === 0}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            {breed.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+      {pets.map((p) => (
+        <Pet name={p.name} animal={p.animal} breed={p.breed} key={p.key} />
+      ))}
+    </div>
+  );
+};
+
+export default SearchParams;
