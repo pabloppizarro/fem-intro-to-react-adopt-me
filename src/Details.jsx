c1ab8a15d22cb3cfca8544bb78c65d63f6e27906
pabloppizarro <pizarro.pablo@outlook.com>
2023-08-22 20:24:09 -0300

    added React Query to pet detail

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,32 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h1>Hi! {id}</h1>;
+  const result = useQuery(["details", id], fetchPet);
+
+  if (result.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h3 className="loader">🐶</h3>
+      </div>
+    );
+  }
+  const pet = result.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
+          <button>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
