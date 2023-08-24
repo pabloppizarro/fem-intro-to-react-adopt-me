a787ad57a9b16697426131a019864664849bf119
pabloppizarro <pizarro.pablo@outlook.com>
2023-08-24 13:29:07 -0300

    added error boundary component in details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 const Details = () => {
   const { id } = useParams();
   const result = useQuery(["details", id], fetchPet);
@@ -30,4 +31,12 @@ const Details = () => {
   );
 };
 
-export default Details;
+function DetailsErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <Details />
+    </ErrorBoundary>
+  );
+}
+
+export default DetailsErrorBoundary;
